da877858c0e8c6c7a7d6b833509b9985cb1822a5
Sehyun Kim <mathshyun@gmail.com>
2024-01-08 16:56:53 +0900

    Added JudgeText

--- Vexatonic_Dash/Assets/Scripts/JudgeBehaviour.cs
+++ Vexatonic_Dash/Assets/Scripts/JudgeBehaviour.cs
@@ -1,11 +1,32 @@
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class JudgeBehaviour : MonoBehaviour
 {
     public static float JudgeDisplayTime = 0.5f;
 
-    private void Start()
+    [SerializeField] private Text text;
+    private float _startTime;
+
+    private void Update()
     {
-        
+        if (Time.time - _startTime >= JudgeDisplayTime) Destroy(gameObject);
+    }
+
+    public void Initialize(JudgementType type)
+    {
+        _startTime = Time.time;
+
+        text.text = type switch
+        {
+            JudgementType.PurePerfect => "Perfect",
+            JudgementType.Perfect => "Perfect",
+            JudgementType.Great => "Great",
+            JudgementType.Good => "Good",
+            JudgementType.Miss => "Miss",
+            JudgementType.Invalid => "Invalid",
+            _ => throw new ArgumentException()
+        };
     }
 }
