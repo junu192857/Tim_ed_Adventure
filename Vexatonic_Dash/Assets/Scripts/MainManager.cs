61f49b9d0205f933199bf50a32e024136a547096
Sehyun Kim <mathshyun@gmail.com>
2024-01-17 15:07:10 +0900

    Added settings buttons onclick functions

--- Vexatonic_Dash/Assets/Scripts/MainManager.cs
+++ Vexatonic_Dash/Assets/Scripts/MainManager.cs
@@ -7,15 +7,29 @@ public class MainManager : MonoBehaviour
     [Header("Main")]
     [SerializeField] private GameObject mainParent;
     
+    [Header("Play")]
+    [SerializeField] private GameObject playParent;
+    
     [Header("Settings")]
     [SerializeField] private GameObject settingsParent;
 
-    [Header("Play")]
-    [SerializeField] private GameObject playParent;
+    [Space(10)]
+    [SerializeField] private GameObject videoSettingsParent;
+
+    [Space(10)]
+    [SerializeField] private GameObject audioSettingsParent;
+    
+    [Space(10)]
+    [SerializeField] private GameObject inputSettingsParent;
+
+    [Space(10)]
+    [SerializeField] private GameObject playSettingsParent;
+
 
     private void Start()
     {
         mainParent.SetActive(true);
+        // TODO: Add animations
     }
 
     public void OnClickMainPlayButton()
@@ -41,24 +55,39 @@ public class MainManager : MonoBehaviour
 #endif
     }
 
+    public void OnClickPlayBackButton()
+    {
+        playParent.SetActive(false);
+        mainParent.SetActive(true);
+        // TODO: Add animations
+    }
+
     public void OnClickSettingsVideoButton()
     {
-        
+        videoSettingsParent.SetActive(true);
+        settingsParent.SetActive(false);
+        // TODO: Add animations
     }
     
     public void OnClickSettingsAudioButton()
     {
-        
+        audioSettingsParent.SetActive(true);
+        settingsParent.SetActive(false);
+        // TODO: Add animations
     }
 
     public void OnClickSettingsInputButton()
     {
-        
+        inputSettingsParent.SetActive(true);
+        settingsParent.SetActive(false);
+        // TODO: Add animations
     }
 
     public void OnClickSettingsPlayButton()
     {
-        
+        playSettingsParent.SetActive(true);
+        settingsParent.SetActive(false);
+        // TODO: Add animations
     }
 
     public void OnClickSettingsBackButton()
@@ -68,10 +97,31 @@ public class MainManager : MonoBehaviour
         // TODO: Add animations
     }
 
-    public void OnClickPlayBackButton()
+    public void OnClickVideoSettingsBackButton()
     {
-        playParent.SetActive(false);
-        mainParent.SetActive(true);
+        videoSettingsParent.SetActive(false);
+        settingsParent.SetActive(true);
+        // TODO: Add animations
+    }
+    
+    public void OnClickAudioSettingsBackButton()
+    {
+        audioSettingsParent.SetActive(false);
+        settingsParent.SetActive(true);
+        // TODO: Add animations
+    }
+    
+    public void OnClickInputSettingsBackButton()
+    {
+        inputSettingsParent.SetActive(false);
+        settingsParent.SetActive(true);
+        // TODO: Add animations
+    }
+    
+    public void OnClickPlaySettingsBackButton()
+    {
+        playSettingsParent.SetActive(false);
+        settingsParent.SetActive(true);
         // TODO: Add animations
     }
 }
