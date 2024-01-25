157920c4d65f128cf7d7080bdaa50c235bcce642
junu123456 <junu123456@naver.com>
2024-01-25 17:55:52 +0900

    인게임 FPS 볼 수 있게 함, 카메라가 캐릭터를 따라감

---
+++ Vexatonic_Dash/Assets/Scripts/Notes/JumpNote.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpNote : Note
+{
+    public override void GetInformationForPlayer(float platformScale, Vector3 startPos)
+    {
+        
+    }
+}
