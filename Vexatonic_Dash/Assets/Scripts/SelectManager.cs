f123b6bd0dc12db11805812b387287b1b3feace4
Sehyun Kim <mathshyun@gmail.com>
2024-02-13 22:11:56 +0900

    Added Shadow

--- Vexatonic_Dash/Assets/Scripts/SelectManager.cs
+++ Vexatonic_Dash/Assets/Scripts/SelectManager.cs
@@ -33,6 +33,9 @@ public class SelectManager : MonoBehaviour
     [SerializeField] private RankIcon rankIcon;
     [SerializeField] private Text highScoreDescriptionText;
     [SerializeField] private Text highScoreValueText;
+
+    [Space(10)]
+    [SerializeField] private Image patternInfoImage;
     [SerializeField] private Text patternInfoText;
 
     [Header("Animator")]
@@ -184,18 +187,6 @@ public class SelectManager : MonoBehaviour
         
         _currentPatternLevel = _songList[_currentIndex].Levels[(int)_currentDifficulty];
         
-        var patternText = _currentDifficulty switch
-        {
-            Difficulty.Easy => "Easy",
-            Difficulty.Hard => "Hard",
-            Difficulty.Vex  => "Vex",
-            _ => throw new System.ArgumentException()
-        };
-
-        patternText += " " + _currentPatternLevel;
-        
-        patternInfoText.text = patternText;
-        
         rankIcon.SetRank(GameManager.GetRank(score));
         rankIcon.ShowAnimation();
         
@@ -209,6 +200,15 @@ public class SelectManager : MonoBehaviour
             highScoreDescriptionText.text = "Progress";
             highScoreValueText.text = $"{progress} %";
         }
+
+        patternInfoImage.color = _currentDifficulty switch
+        {
+            Difficulty.Easy => new Color(0.75f, 1.00f, 0.75f),
+            Difficulty.Hard => new Color(1.00f, 1.00f, 0.75f),
+            Difficulty.Vex  => new Color(1.00f, 0.75f, 0.75f),
+            _ => throw new System.ArgumentException()
+        };
+        patternInfoText.text = _currentDifficulty + " " + _currentPatternLevel;
     }
 
     private void MoveUp()
