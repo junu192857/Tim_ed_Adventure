88925da351cc79fbdb83d4ec5b60ed2606ec7321
Sehyun Kim <mathshyun@gmail.com>
2024-02-13 14:39:22 +0900

    Added Rank image and text

---
+++ Vexatonic_Dash/Assets/Scripts/RankIcon.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RankIcon : MonoBehaviour
+{
+    private static readonly Color[] RankColors =
+    {
+        new(0.9f, 0.7f, 0.1f),  // V Rank
+        new(0.8f, 0.7f, 0.1f),  // S Rank
+        new(0.4f, 0.7f, 0.4f),  // A Rank
+        new(0.2f, 0.6f, 0.7f),  // B Rank
+        new(0.4f, 0.4f, 0.9f),  // C Rank
+        new(0.6f, 0.4f, 0.7f)   // D Rank
+    };
+    
+    [SerializeField] private Image image;
+    [SerializeField] private Text text;
+    private Animator _animator;
+
+    public void SetRank(RankType rank)
+    {
+        image.color = RankColors[(int)rank];
+        text.text = rank.ToString();
+    }
+
+    // Temporary method for testing
+    public void SetRank(int rank)
+    {
+        SetRank((RankType)rank);
+    }
+}
