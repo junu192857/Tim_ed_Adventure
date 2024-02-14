e5bbf929d7efb3df08f7a7271b4ec02aaa56c539
junu123456 <junu123456@naver.com>
2024-02-15 08:19:06 +0900

    캐릭터 회전 버그수정 + 새로운 sprite들 다 수정 완료

--- Vexatonic_Dash/Assets/Scripts/CharacterControl.cs
+++ Vexatonic_Dash/Assets/Scripts/CharacterControl.cs
@@ -68,6 +68,7 @@ public class CharacterControl : MonoBehaviour
         Vector3 stopoverPos;
 
         if (note.angle == 0) {
+            gameObject.transform.localEulerAngles = Vector3.zero;
             while (adjustedTime < playerMovingTime + 0.166f)
             {
                 Vector3 targetPosition = platformNote.startPos * (playerMovingTime - adjustedTime) / playerMovingTime + platformNote.endPos * adjustedTime / playerMovingTime;
@@ -80,6 +81,7 @@ public class CharacterControl : MonoBehaviour
         else {
             forwardMovingTime = playerMovingTime * 0.16f / (platformNote.endPos.x - platformNote.startPos.x);
             stopoverPos = platformNote.startPos + new Vector3(0.16f, 0);
+            gameObject.transform.localEulerAngles = Vector3.zero;
             while (adjustedTime < forwardMovingTime) {
                 Vector3 targetPosition = platformNote.startPos * (forwardMovingTime - adjustedTime) / forwardMovingTime + stopoverPos * adjustedTime / forwardMovingTime;
                 gameObject.transform.position = targetPosition;
