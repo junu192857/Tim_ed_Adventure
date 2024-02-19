a0dc4fa621771b356bdc2692b716f644a75fa711
junu123456 <junu123456@naver.com>
2024-02-19 19:22:14 +0900

    Description 추가
    근데 이 게임 NDM 끝나고 안 할 거잖아

--- Vexatonic_Dash/Assets/Scripts/UIManager.cs
+++ Vexatonic_Dash/Assets/Scripts/UIManager.cs
@@ -116,6 +116,7 @@ public class UIManager : MonoBehaviour
     {
         Debug.Log(GameManager.myManager.selectedSongName);
         //Background part trash-like coded to submit game file as soon as possible. should must be fixed
+        //원래는 레벨 폴더 안에 백그라운드 png 이미지도 있는 게 이상적
         backgroundUI.sprite = GameManager.myManager.selectedSongName switch
         {
             "Savage_Terminal" => backgrounds[0],
